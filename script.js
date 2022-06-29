65d0386a1aa592739b7ecfc0ea26247127f0098c
tpashen <81863630+tpashen@users.noreply.github.com>
2022-06-29 21:11:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('Hello!')
+name=prompt('What is your name?')
+alert('Welcome to my site, '+name)
+
+password=prompt('Enter password')
+if (password=='123'){
+    alert(' Password is correct')
+}
+else{
+    alert('Password is incorrect!')
+    window.close()  
+}
+
+
